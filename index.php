86fdff945d45dbcf395db53648fe7c199123f9fb
hendybua <angriosmarz@gmail.com>
2022-11-29 08:41:29 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam architecto ipsum temporibus nobis laborum distinctio amet sint magnam voluptate delectus, modi quas blanditiis voluptas, harum sunt veniam ea eum nisi.</p>
 </body>
 </html>
